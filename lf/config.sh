624b05c10ed196ed10b3f2559006fca30f020e16
sayonaramemori <sayonaramemori@qq.com>
2024-03-15 10:26:21 +0800

    fix error

--- lf/config.sh
+++ lf/config.sh
@@ -2,36 +2,35 @@
 if [ -e ./lf ]; then
     if [ -e /etc/lf ]
     then
-        echo 'path /etc/lf exists already'
+        echo 'Skipped: path /etc/lf exists already'
     else
         mkdir /etc/lf -p
-        echo 'create /etc/lf'
+        echo 'Done:    create /etc/lf'
     fi
 
     if [ -e /etc/lf/lfrc ]
     then
-        cat ./lfrc >> /etc/lf/lfrc
-        echo 'append configure to lfrc finished, you should chech it manually'
+        echo 'Skipped: configuration file exists already'
     else
         cp ./lfrc /etc/lf/
-        echo 'copy configure to /etc/lf/lfrc finished'
+        echo 'Done:    copy configure to /etc/lf/lfrc finished'
     fi
-    res=`cat ~/.bashrc | grep lfcd`
+    res=`cat /etc/profile | grep lfcd`
     if [ -n "$res" ]
     then
-        echo 'lfcd existed in .bashrc already'
+        echo 'Skipped: lfcd existed in /etc/profile already'
     else
-        cat ./lfcd >> ~/.bashrc
-        echo 'append lfcd to .bashrc finished'
+        cat ./lfcd >> /etc/profile
+        echo 'Done:    append lfcd to /etc/profile finished'
     fi
+    source /etc/profile
     if [ -e /usr/bin/lf ]
     then 
-        echo '/usr/bin/lf is already exist'
+        echo 'Skipped: /usr/bin/lf is already exist'
     else
         cp ./lf /usr/bin/
-        echo 'copy lf to /usr/bin finished'
+        echo 'Done:    copy lf to /usr/bin finished'
     fi 
-    source ~/.bashrc
     echo "Config OK, use ra to run lfcd, use q to return to the terminal"
 else
     echo "Please download lf binary file first and ensure compatible"
--- vim-config/vim_config.sh
+++ vim-config/vim_config.sh
@@ -1,15 +1,26 @@
 user=`whoami`
 echo "Current operation for user: $user"
 read -p 'Please ensure the current user:' user
-echo "Now for $user"
-prefix="/home/$user"
-cp ./.vimrc ${prefix}/.vimrc
-
-if [ -e ${prefix}/.vim/autoload ]
+query=`id $user | grep no`
+if [ -n $query ]
 then
-    cp ./plug.vim ${prefix}/.vim/autoload/
+    echo 'no such user, stop running'
 else
-    mkdir ${prefix}/.vim/autoload -p
-    cp ./plug.vim ${prefix}/.vim/autoload/
+    echo "Now for $user"
+    if [ $user = 'root' ]
+    then
+        prefix="/$user"
+    else
+        prefix="/home/$user"
+    fi
+    cp ./.vimrc ${prefix}/.vimrc
+
+    if [ -e ${prefix}/.vim/autoload ]
+    then
+        cp ./plug.vim ${prefix}/.vim/autoload/
+    else
+        mkdir ${prefix}/.vim/autoload -p
+        cp ./plug.vim ${prefix}/.vim/autoload/
+    fi
 fi
 
