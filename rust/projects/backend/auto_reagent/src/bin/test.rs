e9e55f70100452df3db569f84d587338c8e2f966
javaee <1342733420>
2024-06-21 18:03:35 +0800

    kill rust

--- rust/projects/backend/auto_reagent/src/bin/test.rs
+++ rust/projects/backend/auto_reagent/src/bin/test.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer, HttpResponse};
 use actix_cors::Cors;
 // use std::env;
 
@@ -10,15 +10,17 @@ async fn main() -> std::io::Result<()> {
     //let ip:String = env::var("IP").unwrap();
     HttpServer::new(|| {
        let cors = Cors::default()
-             .allowed_origin("http://localhost:5173")
+             .allow_any_origin()
              .allowed_methods(vec!["GET", "POST"])
+             .allowed_headers(vec![actix_web::http::header::AUTHORIZATION, actix_web::http::header::ACCEPT])
+             .allowed_header(actix_web::http::header::CONTENT_TYPE)
              .max_age(3600);
         App::new()
             .wrap(cors)
             .service(findlast)
             .service(greet)
     })
-    .bind(("localhost", 8080))?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
