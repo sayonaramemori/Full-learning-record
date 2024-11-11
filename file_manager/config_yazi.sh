50f86ee51ee13223d9eb019752b404c55d5b19ea
Ruster <lover@cpp.com>
2024-11-11 18:58:08 +0800

    finish config nvim and yazi

--- file_manager/config_yazi.sh
+++ file_manager/config_yazi.sh
@@ -1,11 +1,22 @@
-echo "Installing Yazi, Rust Environment is needed" &&
-cargo install --locked yazi-fm yazi-cli &&
-mkdir ~/.config/yazi -p  &&
-cp ./keymap.toml ~/.config/yazi/keymap.toml &&
-cp ./yazi.toml   ~/.config/yazi/yazi.toml &&
-cp ./init.lua    ~/.config/yazi/init.lua &&
-ya pack -a yazi-rs/plugins:full-border &&
-ya pack -a h-hg/yamb
+# echo "Installing Yazi, Rust Environment is needed" &&
+# cargo install --locked yazi-fm yazi-cli &&
+# mkdir ~/.config/yazi -p  &&
+# cp ./keymap.toml ~/.config/yazi/keymap.toml &&
+# cp ./yazi.toml   ~/.config/yazi/yazi.toml &&
+# cp ./init.lua    ~/.config/yazi/init.lua &&
+# ya pack -a yazi-rs/plugins:full-border &&
+# ya pack -a h-hg/yamb &&
+cat << 'EOF' >> ~/.bashrc
+function ra() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+} 
+EOF
 
+source ~/.bashrc
 
 
--- vim-config/nvim_config.sh
+++ vim-config/nvim_config.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz &&
+sudo rm -rf /opt/nvim &&
+sudo tar -C /opt -xzf nvim-linux64.tar.gz &&
+cat << 'EOF' >> ~/.bashrc
+export PATH="$PATH:/opt/nvim-linux64/bin"
+EOF
+source ~/.bashrc
 mkdir -p ~/.config/nvim
 rm -rf ~/.config/nvim/*
 cp ./init.lua ~/.config/nvim/init.lua
